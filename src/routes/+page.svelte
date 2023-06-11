e6fd0e6f142076904f9d0a9270db2e01d2832651
Mayu B <mayu.barajas99@gmail.com>
2023-06-10 21:41:27 -0700

    add tree

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1 class="text-3xl font-bold underline">Hi good evening</h1>
+<h1 class="text-3xl font-bold underline p-10">Hi good evening</h1>
+
+<div class="card w-96 bg-base-300 shadow-xl m-auto">
+    <div class="card-body">
+      <h2 class="card-title">Tree</h2>
+      <p>big tall tree</p>
+    </div>
+    <figure><img src="/images/IMG_3222-cleaned.jpg" alt="Shoes" /></figure>
+  </div>
